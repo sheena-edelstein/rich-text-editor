75fd7faabb3dc1912ddb1d6d17194ed61b318431
Maria Clara Nascimento Silva <mariaclarasilvanascimento2006@gmail.com>
2025-02-05 22:19:46 -0300

    Add files via upload

---
+++ texteditor.js
@@ -0,0 +1,14 @@
+let output = document.getElementById('output');
+let buttons = document.getElementsByClassName('tool--btn');
+for (let btn of buttons) {
+    btn.addEventListener('click', () =>{
+        let cmd = btn.dataset['command'];
+        if (cmd === 'createlink'){
+            let url = prompt("Enter the link here: ", "https:\/\/");
+            document.execCommand(cmd, false, url);
+        }
+        else {
+            document.execCommand(cmd, false, null);
+        }
+    })
+}
